7156d585af93adf0bc7f490865d19dcce7cf9d14
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-09-21 22:15:52 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 import os
-
+import sys
 
 setup(
     name='photonic',
     version="1.0",
     author='Jakob S. Kottmann',
     author_email='jakob.kottmann@gmail.com',
-    install_requires= ['tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila' ]
-    #packages=find_packages(where='src'),
-    #package_dir={'': 'src'}
+    install_requires=['tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila' ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
+        '': [os.path.join('src')]
+    }
 )
