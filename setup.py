6f1199a138c744340d892f3d0049da6b97b1d684
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-09-21 22:11:09 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="1.0",
     author='Jakob S. Kottmann',
     author_email='jakob.kottmann@gmail.com',
-    install_requires= ['tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila' ],
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'}
+    install_requires= ['tequila @ git+https://github.com/aspuru-guzik-group/tequila.git@master-setup-fix#egg=tequila' ]
+    #packages=find_packages(where='src'),
+    #package_dir={'': 'src'}
 )
